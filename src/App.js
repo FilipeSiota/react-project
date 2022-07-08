bf9e30db7a4ad98395100eb89a308e6c21d44ade
Filipe Mallmann Siota <filipesiota@gmail.com>
2022-07-08 19:10:52 -0300

    Aula 3 - Entendendo o JSX

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import './App.css';
 
-function App() {
+function App() { // main function
+
+  const name = 'Filipe'
+
+  const newName = name.toUpperCase()
+
+  function sum(a, b) {
+    return a + b
+  }
+
+  const url = 'https://via.placeholder.com/150'
+
   return (
+    // Classe = className
     <div className="App">
-      <h1>Olá mundo!</h1>
+      <h1>Alterando o JSX</h1>
+      <p>Olá, {newName}!</p>
+      <p>Soma: {2 + 2}</p>
+      <p>Soma por função: {sum(2, 5)}</p>
+      <img src={url} alt='Minha imagem' />
     </div>
   );
 }
